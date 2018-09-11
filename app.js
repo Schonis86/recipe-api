2430c18572ee3ed4ac99da2d00eae4df560c4d5a
Calle Schönberg <calleschonberg@gmail.com>
2018-09-11 17:20:44 +0200

    small changes

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const recipeRoutes = require('./api/routes/recipes');
 const productRoutes = require('./api/routes/products');
 
+app.use(morgan('dev'));
 
 app.use('recipes', recipeRoutes);
 app.use('products', productRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
