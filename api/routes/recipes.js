8b6135009ac4836f6b0a7facfc92a19a0776cf79
Calle Schönberg <calleschonberg@gmail.com>
2018-09-11 18:24:58 +0200

    small changes

--- api/routes/recipes.js
+++ api/routes/recipes.js
@@ -8,6 +8,12 @@ router.get('/', (req, res, next) =>{
 });
 
 router.post('/', (req, res, next) =>{
+    const recipe = {
+        recipeName: req.body.recipeName,
+        category: req.body.category,
+        description: req.body.description,
+        ingredients: req.body.ingredients
+    }
     res.status(200).json({
         message: 'Handling Post request to /recipes'
     });
--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser'); 
 
 const recipeRoutes = require('./api/routes/recipes');
 const productRoutes = require('./api/routes/products');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((res, req, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'origin  , X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET ');
+        return res.status(200).json({})
+    }
+    next();
+})
+
 
 app.use('recipes', recipeRoutes);
 app.use('products', productRoutes);
